55aeab2fe3895ef85cff00220e3a1deac25db2ea
Single <1197826934@qq.com>
2024-01-06 19:50:59 +0800

    初始化目录、安装依赖、组件库

---
+++ src/pages/community/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="community">community</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
---
+++ src/pages/mine/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    @tap="
+      memberStore.setProfile({
+        nickname: '用户33033',
+      })
+    "
+  >
+    保存用户信息
+  </button>
+  <button @tap="memberStore.clearProfile()">清理用户信息</button>
+</template>
+
+<script lang="ts" setup>
+import { useMemberStore } from '@/stores'
+
+const memberStore = useMemberStore()
+</script>
+
+<style scoped></style>
