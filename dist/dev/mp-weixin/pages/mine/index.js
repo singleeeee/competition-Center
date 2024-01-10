85787d890b6197eb13b5b29c31ed39fae6b0c665
Single <1197826934@qq.com>
2024-01-10 11:55:09 +0800

    拦截器部分初始化

--- dist/dev/mp-weixin/pages/mine/index.js
+++ dist/dev/mp-weixin/pages/mine/index.js
@@ -1,17 +1,27 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
 require("../../stores/index.js");
+const utils_http = require("../../utils/http.js");
 const stores_modules_member = require("../../stores/modules/member.js");
 const _sfc_main = /* @__PURE__ */ common_vendor.defineComponent({
   __name: "index",
   setup(__props) {
     const memberStore = stores_modules_member.useMemberStore();
+    const getData = async () => {
+      const res = await utils_http.http({
+        method: "GET",
+        url: "/home/banner",
+        header: {}
+      });
+      console.log("请求成功！", res);
+    };
     return (_ctx, _cache) => {
       return {
         a: common_vendor.o(($event) => common_vendor.unref(memberStore).setProfile({
           nickname: "用户33033"
         })),
-        b: common_vendor.o(($event) => common_vendor.unref(memberStore).clearProfile())
+        b: common_vendor.o(($event) => common_vendor.unref(memberStore).clearProfile()),
+        c: common_vendor.o(getData)
       };
     };
   }
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -9,12 +9,22 @@
     保存用户信息
   </button>
   <button @tap="memberStore.clearProfile()">清理用户信息</button>
+  <button @tap="getData">请求测试</button>
 </template>
 
 <script lang="ts" setup>
 import { useMemberStore } from '@/stores'
-
+import { http } from '@/utils/http'
 const memberStore = useMemberStore()
+// 测试请求
+const getData = async () => {
+  const res = await http<string[]>({
+    method: 'GET',
+    url: '/home/banner',
+    header: {},
+  })
+  console.log('请求成功！', res)
+}
 </script>
 
 <style scoped></style>
